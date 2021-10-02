995113b97378b3626128e0ba4d68f861506edec2
Andy Giroux <andy.r.giroux@gmail.com>
2021-10-02 06:59:16 -0400

    initial commit

---
+++ src/pages/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function WelcomePage() {
+    return (
+        <Layout>
+            <h1>Just a test dude</h1>
+        </Layout>
+    );
+}
